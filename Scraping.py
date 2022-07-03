5ba0b719b8ede23673d42d473bc32a96b0cd072b
lauren1478 <lauren.rishel@utexas.edu>
2022-07-03 17:35:18 -0500

    commit

--- Scraping.py
+++ Scraping.py
@@ -12,7 +12,8 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-
+    hemisphere_image_urls = hemisphere(browser)
+    
     # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
@@ -98,3 +99,35 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+
+# ##Hemispheres
+def mars_hemispheres(browser):
+    url = 'https://marshemispheres.com'
+    browser.visit(url)
+
+   
+    hemisphere_image_urls = []
+
+    for i in range(4):
+        hemispheres = {}
+    
+        browser.find_by_css('h3')[i].click()
+        element = browser.links.find_by_text('Sample').first
+    
+        img_url = element['href']
+    
+        title = browser.find_by_css('h2.title').text
+    
+        hemispheres['img_url'] = img_url
+        hemispheres['title'] = title
+
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+        hemisphere_image_urls
+
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
